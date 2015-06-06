99207cc84ab563b21287692341fb1cd20a755c6b
AJ Richardson <aj.ric.ar+git@gmail.com>
2015-06-06 16:22:00 -0400

    Added the JSON code for real this time

---
+++ FileManagement.Json/JsonSerializer.cs
@@ -0,0 +1,68 @@
+﻿using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace FileManagement.Json
+{
+    /// <summary>
+    /// Serializes and deserializes objects in JSON format.
+    /// </summary>
+    public class JsonSerializer : ISerializer
+    {
+        private JsonSerializerSettings settings;
+
+        /// <summary>
+        /// Creates a new <see cref="JsonSerializer"/> instance.
+        /// </summary>
+        public JsonSerializer()
+            : this(null)
+        { }
+
+        /// <summary>
+        /// Creates a new <see cref="JsonSerializer"/> instance.
+        /// </summary>
+        /// <param name="settings">The JSON serialization settings to use.</param>
+        public JsonSerializer(JsonSerializerSettings settings)
+        {
+            this.settings = settings;
+        }
+
+        #region ISerializer Members
+
+        /// <summary>
+        /// Serializes an object.
+        /// </summary>
+        /// <typeparam name="T">The type of object to serialize.</typeparam>
+        /// <param name="stream">The stream to which the object will be serialized.</param>
+        /// <param name="encoding">The character encoding to use.</param>
+        /// <param name="obj">The object to serialize.</param>
+        public void Serialize<T>(Stream stream, Encoding encoding, T obj)
+        {
+            using (var writer = new StreamWriter(stream, encoding, 1024, true))
+            using (var jsonWriter = new JsonTextWriter(writer))
+            {
+                var serializer = new Newtonsoft.Json.JsonSerializer();
+                serializer.Serialize(jsonWriter, obj);
+            }
+        }
+
+        /// <summary>
+        /// Deserializes an object.
+        /// </summary>
+        /// <typeparam name="T">The type of object to deserialize</typeparam>
+        /// <param name="stream">The stream from which the object will be deserialized.</param>
+        /// <param name="encoding">The character encoding to use.</param>
+        /// <returns>The deserialized object.</returns>
+        public T Deserialize<T>(Stream stream, Encoding encoding)
+        {
+            using (var reader = new StreamReader(stream, encoding, false, 1024, true))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                var serializer = new Newtonsoft.Json.JsonSerializer();
+                return serializer.Deserialize<T>(jsonReader);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ FileManagement.Test/JsonSerializerTests.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using System.Text;
+using FileManagement.Json;
+using NUnit.Framework;
+
+namespace FileManagement.Test
+{
+    [TestFixture]
+    public class JsonSerializerTests
+    {
+        [Test]
+        public void CanSerialize()
+        {
+            using (var ms = new MemoryStream())
+            {
+                var serializer = new JsonSerializer();
+                var file = new TestFile { TestProperty = "test" };
+
+                serializer.Serialize(ms, Encoding.UTF8, file);
+
+                var s = Encoding.UTF8.GetString(ms.GetBuffer(), 0, (int)ms.Length);
+                // The first character seems to be something that indicates the endocidng. Ignore it.
+                if (s.Length > 0 && s[0] > 255)
+                    s = s.Substring(1);
+                Assert.That(s, Is.EqualTo(@"{""TestProperty"":""test""}"));
+            }
+        }
+
+        [Test]
+        public void CanDeserialize()
+        {
+            var buffer = Encoding.UTF8.GetBytes(@"{""TestProperty"":""test""}");
+            TestFile testFile;
+            var serializer = new JsonSerializer();
+
+            using (var ms = new MemoryStream(buffer))
+            {
+                testFile = serializer.Deserialize<TestFile>(ms, Encoding.UTF8);
+                // Make sure the stream is still open.
+                // The caller probably is not expecting us to close the stream, especially if this is a network stream or something.
+                // It is the caller's responsibility to close the stream.
+                Assert.That(ms.CanRead);
+            }
+
+            Assert.That(testFile, Is.Not.Null);
+            Assert.That(testFile.TestProperty, Is.EqualTo("test"));
+        }
+    }
+}
